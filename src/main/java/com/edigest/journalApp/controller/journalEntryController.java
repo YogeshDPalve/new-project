86dd7a3044846558d1fd4cbfd939734153a0406a
Yogesh Palve <dnyyogesh@gmail.com>
2025-06-06 23:32:05 +0530

    first commit

---
+++ src/main/java/com/edigest/journalApp/controller/journalEntryController.java
@@ -0,0 +1,29 @@
+package com.edigest.journalApp.controller;
+
+import com.edigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class journalEntryController {
+    private Map<Long,JournalEntry> journalEntries = new HashMap();
+    @GetMapping("/get") // Get method
+    public List<JournalEntry> getAll(){
+            return new ArrayList<>( journalEntries.values());
+    }
+
+    @PostMapping("/post")  // Post method
+    public boolean createEntry(@RequestBody JournalEntry myEntry ){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+}
